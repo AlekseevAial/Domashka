e5c0a5405b790877debf64e84df757b54c9547f4
AlekseevAial <ayal-moby@mail.ru>
2022-03-26 21:13:51 +0800

    input

--- Alekseev_Aial_dz_1.py
+++ Alekseev_Aial_dz_1.py
@@ -10,5 +10,5 @@ minutes = seconds // seconds_in_minute
 seconds = seconds - (minutes * seconds_in_minute)
 print("{0:.0f} days, {1:.0f} hours, {2:.0f} minutes, {3:.0f} seconds.".format(
     days, hours, minutes, seconds))
-
+input()
 
